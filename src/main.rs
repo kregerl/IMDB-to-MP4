e7dad309580c8f1d6ce70461929ef8154909f3c0
kregerl <loucaskreger@gmail.com>
2024-02-16 15:31:29 -0500

    Revert "Added a TUI" - moved to new branch
    This reverts commit 00ef12e857974da7da29ad52c53426c4c6a01adb.

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,51 @@
 use std::{
     fs::{File, OpenOptions},
-    io::{self, stdout, Write},
+    io::{self, Write},
     path::Path,
-    sync::{Arc, Mutex, RwLock},
     thread,
 };
 
-use clap::{Arg, Command, Parser};
-use crossterm::{
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
-};
-use ratatui::{backend::CrosstermBackend, Terminal};
-use reqwest::Url;
-use tui::{run_app, App};
+use clap::Parser;
 use vidsrc::{download_series, download_video_manifest, request_video_page, Manifest, Video};
-mod text_input;
-mod tui;
 mod vidsrc;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    from_url: String,
-    #[arg(long)]
-    from_file: String,
+    movie_name: String,
+    m3u8_path: String,
 }
 
 // No threading: cargo run  61.09s user 6.66s system 32% cpu 3:29.55 total
 // 4 Threads: cargo run  79.47s user 13.14s system 187% cpu 49.327 total
 // 8 Threads: cargo run  139.71s user 57.83s system 346% cpu 56.972 total
 fn main() {
-    let args = Command::new("IMDB2MP4")
-        .arg(
-            Arg::new("from_url")
-                .short('u')
-                .long("from-url")
-                .exclusive(true),
-        )
-        .arg(
-            Arg::new("from_file")
-                .short('f')
-                .long("from-file")
-                .exclusive(true),
-        )
-        .get_matches();
-
-    if let Some(url_str) = args.get_one::<String>("from_url") {
-        let url_str = url_str.strip_suffix("/").unwrap();
-        let url = Url::parse(url_str).unwrap();
-        let last_segment = url.path_segments().unwrap().last();
-        if let Some(imdb_id) = last_segment {
-            let video = request_video_page(imdb_id).unwrap();
-
-            enable_raw_mode().unwrap();
-            stdout().execute(EnterAlternateScreen).unwrap();
-            let mut terminal = Terminal::new(CrosstermBackend::new(stdout())).unwrap();
-
-            match video {
-                Video::Movie(source) => {
-                    let app = App::new();
-                    run_app(&mut terminal, app, vec![source]).unwrap();
-                    // download_video(vec![manifest]).unwrap();
-                }
-                Video::Series(episodes) => {
-                    let manifests = download_series(&episodes, |episode| {
-                        // episode.episode == "1" && episode.season == "1"
-                        // true
-                        false
-                    })
-                    .unwrap();
-                    download_video(manifests).unwrap();
-                }
-            }
+    // kung fu panda 3
+    // let imdb = "tt2267968";
+    // fosters
+    let imdb = "tt0419326";
+    // Deadpool
+    // let imdb = "tt1431045";
+    // Star Wars
+    // let imdb = "tt0120915";
+    let video = request_video_page(imdb).unwrap();
+    match video {
+        Video::Movie(source) => {
+            let manifest = download_video_manifest(&source.title, &source.data_iframe).unwrap();
+            download_video(vec![manifest]).unwrap();
         }
-        disable_raw_mode().unwrap();
-        stdout().execute(LeaveAlternateScreen).unwrap();
+        Video::Series(episodes) => {
+            let manifests = download_series(&episodes, |episode| {
+                // episode.episode == "1" && episode.season == "1"
+                true
+            })
+            .unwrap();
+            download_video(manifests).unwrap();
+        }
     }
-
-    if let Some(file) = args.get_one::<String>("from_file") {
-        todo!();
-    }
-
-    return;
-    // // kung fu panda 3
-    // // let imdb = "tt2267968";
-    // // fosters
-    // let imdb = "tt0419326";
-    // // Deadpool
-    // // let imdb = "tt1431045";
-    // // Star Wars
-    // // let imdb = "tt0120915";
-    // let video = request_video_page(imdb).unwrap();
-    // match video {
-    //     Video::Movie(source) => {
-    //         let manifest = download_video_manifest(&source.title, &source.data_iframe).unwrap();
-    //         download_video(vec![manifest]).unwrap();
-    //     }
-    //     Video::Series(episodes) => {
-    //         let manifests = download_series(&episodes, |episode| {
-    //             // episode.episode == "1" && episode.season == "1"
-    //             // true
-    //             false
-    //         })
-    //         .unwrap();
-    //         download_video(manifests).unwrap();
-    //     }
-    // }
 }
 
-pub fn download_video(manifests: Vec<Manifest>) -> io::Result<()> {
+fn download_video(manifests: Vec<Manifest>) -> io::Result<()> {
     for manifest in manifests {
         let movie_file_name = format!("{}.mp4", manifest.title);
         let movie_path = Path::new(&movie_file_name);
@@ -130,7 +65,6 @@ pub fn download_video(manifests: Vec<Manifest>) -> io::Result<()> {
             let mut thread_handles = Vec::with_capacity(MAX_CHUNKS);
             let chunks = urls.chunks((urls.len() as f32 / MAX_CHUNKS as f32).ceil() as usize);
             for (id, chunk) in chunks.enumerate() {
-                // let pct = percentages[id].clone();
                 thread_handles
                     .push(scope.spawn(move || download_movie_chunk(id + 1, chunk).unwrap()))
             }
@@ -162,10 +96,6 @@ fn download_movie_chunk(id: usize, url_chunk: &[String]) -> Result<Vec<u8>, Stri
 
         let bytes = response.bytes().unwrap();
         all_bytes.append(&mut bytes.to_vec());
-
-        // let mut lock = percent.write();
-        // let x = lock.as_deref_mut().unwrap();
-        // *x = ((index + 1) / url_chunk.len()) as u16;
     }
 
     Ok(all_bytes)
